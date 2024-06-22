cf3a56bddba3a47866b59a82d58cb23c2f194526
Francis Bond <bond@ieee.org>
2024-06-22 16:14:53 +0200

    added program, minor updates

--- web/routes.py
+++ web/routes.py
@@ -15,10 +15,13 @@ current_directory = os.path.abspath(os.path.dirname(__file__))
 nav = {
     'index': {"name":"HPSG 2024",
              'desc':""},
-    'cfp3': {"name": "Call For Papers\n (main)",
-             'desc':''},
     'workshop': {"name": "Workshop on Formal Approaches\n to Under-resourced Languages",
                  'desc':'Workshop on Formal Approaches to Under-resourced Languages'},
+    'program': {"name": "Program",
+             'desc':'Shared program'},
+
+    'cfp3': {"name": "Call For Papers\n (main)",
+             'desc':''},
     'wcfp3': {"name": "Call For Papers\n (workshop)",
              'desc':''},
     'local': {"name":"Local Information",
